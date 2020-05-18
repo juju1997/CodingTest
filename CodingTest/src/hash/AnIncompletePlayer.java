df58ce72b97bb23b2eac3f6b361da97211e720aa
juju <juju@localhost>
2020-05-19 02:13:15 +0900

    2020-05-19 02:10 프로그래머스 문제풀이(완주하지 못한 자 향상된 for문으로 변경)

--- CodingTest/src/hash/AnIncompletePlayer.java
+++ CodingTest/src/hash/AnIncompletePlayer.java
@@ -15,23 +15,23 @@ public class AnIncompletePlayer {
 		String answer = "";
 		HashMap<String, Integer> hash = new HashMap<>();	//key, value 로 저장할 hash map
 		
-		for(int i=0; i<participant.length; i++) {			//참가자의 길이만큼 돌린다.
-			hash.put(participant[i], hash.getOrDefault(participant[i], 0) + 1);	
+		for(String par : participant) {			//참가자의 길이만큼 돌린다.
+			hash.put(par, hash.getOrDefault(par, 0) + 1);	
 			//hash에 참가자를 한명씩 넣는다. 
 			//getOrDefault : 키 값을 검색하여 있으면 값 반환 없으면 기본 값 , 즉 없으면 1, 있으면 1을 가져와서 2가 된다.
 		}
 		
-		for(int i=0; i<completion.length; i++) {			//완주자의 길이만큼 돌린다. 
-			hash.put(completion[i], hash.getOrDefault(completion[i], 0) - 1);
+		for(String com : completion) {			//완주자의 길이만큼 돌린다. 
+			hash.put(com, hash.getOrDefault(com, 0) - 1);
 			//hash에 완주자를 한명씩 넣는다.
 			// 위 코드와 다른 점은 이미 있는 값이면 1이 저장되어있으니 -1 해서 해당 키의 값을 0으로 바꾼다.
 			// 완주자에 없어서 -1이 안된 참가자는 값이 1이 될 것이다.
 			// * 동명이인은 참가자에서 2까지 올린 후 둘중 하나가 못들어오면 1, 둘 다 들어오면 0 으로 같은 방식으로 처리 가
 		}
 		
-		for(int i=0; i<participant.length; i++) {	//참가자의 길이만큼 돌린다.
-			if(hash.get(participant[i]) == 1) {		//마찬가지로 참가자이름의 키에 값이 1이면 완주를 못한 사람이 된다.
-				answer = participant[i];
+		for(String par : participant) {	//참가자의 길이만큼 돌린다.
+			if(hash.get(par) == 1) {		//마찬가지로 참가자이름의 키에 값이 1이면 완주를 못한 사람이 된다.
+				answer = par;
 			}
 		}
 		return answer;
