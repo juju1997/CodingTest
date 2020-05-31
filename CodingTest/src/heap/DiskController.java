fb9725f53cdd5a22fa50165babba1aff56ceef3f
juju <gudwls671214@gmail.com>
2020-05-31 19:19:21 +0900

    2020-05-31 힙-디스크컨트롤러 해결중

---
+++ CodingTest/src/heap/DiskController.java
@@ -0,0 +1,65 @@
+package heap;
+
+import java.util.*;
+
+public class DiskController {
+
+	public static void main(String[] args) {
+		int[][] jobs = {{0,3}, {1,9}, {2,6}};
+//				9
+//		int[][] jobs = {{24, 10}, {18, 39}, {34, 20}, {37, 5}, {47, 22}, {20, 47}, {15, 2}, {15, 34}, {35, 43}, {26, 1}};
+//				74
+//		int[][] jobs = {{24, 10}, {18, 39}, {34, 20}, {37, 5}, {47, 22}, {20, 47}, {15, 34}, {15, 2}, {35, 43}, {26, 1}}
+//				74
+		solution(jobs);
+	}
+	
+	
+	
+	
+	
+	/*
+	 * jobs{}{}에 있는 작업들은 요청시간 순으로 정렬되어 있지 않음
+		같은 요청시간을 갖는 작업들이 2개 이상 존재할 수 있음
+	 * */
+	
+	public static int solution(int[][] jobs) {
+        int answer = 0;
+        
+        PriorityQueue<Disk> before = new PriorityQueue<>();	//프로세싱 전 
+        List<Disk> ing = new LinkedList<>();	//프로세싱 중 
+        List<Integer> after = new ArrayList<>();	//프로세싱 끝 
+        
+        for(int i=0; i<jobs.length; i++) {
+        	before.offer(new Disk(jobs[i][0], jobs[i][1]));
+        }
+        
+        
+        
+        return answer;
+    }
+
+}
+class Disk implements Comparable<Disk>{
+	int priority;
+	int time;
+	Disk(int priority, int time){
+		this.priority = priority;
+		this.time = time;
+	}
+	@Override
+	public int compareTo(Disk o) {
+		if(this.time > time) {
+            return 1;
+        } else if (this.time < time) {
+            return -1;
+        } else {
+            return 0;
+        }
+	}
+	@Override
+	public String toString() {
+		return "Disk [priority=" + priority + ", time=" + time + "]";
+	}
+	
+}
