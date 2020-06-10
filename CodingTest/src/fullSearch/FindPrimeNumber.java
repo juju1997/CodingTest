4bc25c8ec87024361386d0906a0cd1989e9180d0
juju <gudwls671214@gmail.com>
2020-06-10 20:58:20 +0900

    독학사 공부로 인한 커밋을 위한 커밋

--- CodingTest/src/fullSearch/FindPrimeNumber.java
+++ CodingTest/src/fullSearch/FindPrimeNumber.java
@@ -21,7 +21,8 @@ public class FindPrimeNumber {
 	제한사항
 	numbers는 길이 1 이상 7 이하인 문자열입니다.
 	numbers는 0~9까지 숫자만으로 이루어져 있습니다.
-	013은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.
+	013은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.	
+	커폴
 	 * */
 	public static int solution(String numbers) {
         int answer = 0;
