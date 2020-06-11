3d8343092f741fe15c89a4fed8324bd731b89c03
juju <gudwls671214@gmail.com>
2020-06-11 16:48:48 +0900

    2020-06-11 완전탐색 - 숫자야구 완료

--- CodingTest/src/fullSearch/NumberBaseBall.java
+++ CodingTest/src/fullSearch/NumberBaseBall.java
@@ -38,7 +38,7 @@ public class NumberBaseBall {
         for(int i=1; i<=9; i++) {
         	for(int j=1; j<=9; j++) {
         		for(int k=1; k<=9; k++) {
-        			if(i==j & j==k) continue;
+        			if(i==j || j==k || i==k) continue;
         			int comp = 0;
         			for(int[] base : baseball) {
         				int one = base[0]/100;
