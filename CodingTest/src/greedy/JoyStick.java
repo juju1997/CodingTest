19c1aa2c66ec9edcf31b2cab02e04d2730878cf4
juju <gudwls671214@gmail.com>
2020-07-07 19:19:36 +0900

    2020-07-07 탐욕법 - 조이스틱 완

--- CodingTest/src/greedy/JoyStick.java
+++ CodingTest/src/greedy/JoyStick.java
@@ -1,6 +1,5 @@
 package greedy;
 
-import java.util.*;
 
 public class JoyStick {
 
@@ -11,41 +10,31 @@ public class JoyStick {
 //		23
 //		String name = "JAZ";
 //		11
-		solution(name);
+		System.out.println(solution(name));
 	}
 	
 	public static int solution(String name) {
-        int answer = -1;
-        // A = 65
-        //ASCII 65 ~ 90
-        int mid  = (65+90)/2;
-        int A = 65;
-        char[] arr = name.toCharArray();
-        int[] intArr = new int[arr.length];
-        for(int i=0; i<intArr.length; i++) {
-        	intArr[i] = (int)arr[i];
-        }
-        Queue<String> q = new LinkedList<>();
-        //정상진행 right 비정상진행 left 
-        for(int i=1; i<intArr.length; i++) {
-        	if(intArr[i]!=A) {
-        	}else {
-        	}
+        int ans = 0;
+        for(int i = 0 ; i < name.length() ; i++) {
+            if(name.charAt(i)!= 'A') {
+                int up = name.charAt(i) - 'A';
+                int down = 1 + 'Z' - name.charAt(i);
+                ans += (up < down)? up : down;
+            }
         }
         
-        for(int i=0; i<arr.length; i++) {
-        	if(intArr[i]==65) { 
-        		answer++;
-        		continue;
-        	}else {
-        		if(intArr[i] < mid) {	//주어진 값이 중간값이랑 가까울 A까지 -하면서 가야함 
-        			answer += (intArr[i]-A);
-        		}else {	//주어진 값이 중간값보다 멀때 +하면서 가야함
-        		}
-        	}
-        	answer++;
+        int minMove = name.length() - 1;
+        for(int i = 0 ; i < name.length() ; i++) {
+            if(name.charAt(i) != 'A') {
+                int next = i+1;
+                while(next < name.length() && name.charAt(next) == 'A') {
+                    next++;
+                }
+                int move = 2 * i + name.length() - next;
+                minMove = Math.min(move, minMove);
+            }
         }
-        return answer;
-    }
-
+        
+        return ans + minMove;
+	}
 }
