8f74786eb854502253a9d78e713d84d8bbaf68d5
MyHome <MyHome@DESKTOP-6ND2P8F>
2020-07-23 13:28:56 +0900

    2020-07-23 같은 숫자는 싫어 완료

---
+++ CodingTest/src/practice/NoSameNumber.java
@@ -0,0 +1,42 @@
+package practice;
+
+import java.util.*;
+
+public class NoSameNumber {
+
+	public static void main(String[] args) {
+//		int[] arr = {1,1,3,3,0,1,1};	//1,3,0,1
+		int[] arr = {4,4,4,3,3};	//4,3	
+		solution(arr);
+	}
+	/*
+	 * 배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다. 
+	 * 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다. 
+	 * 단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
+
+		arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
+		arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
+		배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
+		
+		제한사항
+		배열 arr의 크기 : 1,000,000 이하의 자연수
+		배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
+	 * */
+	
+	    public static int[] solution(int []arr) {
+	        List<Integer> list = new ArrayList<>();
+	        int com = -1;
+	        for(int i : arr) {
+	        	if(i == com){
+	        	}else{
+	        		list.add(i);
+	        		com = i;
+	        	}
+	        }
+	        int[] answer = new int[list.size()];
+	        for(int i=0; i<answer.length; i++){
+	        	answer[i] = list.get(i);
+	        }
+	        return answer;
+	    }
+}
